b2aee344c2eb8a07bd2ab56f77fa12130096337d
develop-y-minami <developyminami@gmail.com>
2024-01-23 22:06:53 +0900

    commit

--- app/Repositorys/LineTransitionRepositoryInterface.php
+++ app/Repositorys/LineTransitionRepositoryInterface.php
@@ -12,8 +12,8 @@ interface LineTransitionRepositoryInterface
      * LINE数推移情報を取得
      * 
      * @param int    serviceProviderId  サービス提供者情報ID
-     * @param string transitionDateFrom 日付FROM
-     * @param string transitionDateTo   日付TO
+     * @param string transitionDateFrom 日付：FROM
+     * @param string transitionDateTo   日付：TO
      * @return Collection LINE数推移情報
      */
     public function findByServiceProvider($serviceProviderId, $transitionDateFrom = null, $transitionDateTo  = null);
